35b6903c1410b284dd14d3accd2dd8c8bf4b768c
naman <namantam1@gmail.com>
2020-07-14 02:44:15 +0530

    configured s3 bucket

--- doubtapi/settings.py
+++ doubtapi/settings.py
@@ -148,8 +148,8 @@ MEDIA_URL = '/media/'
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_STORAGE_BUCKET_NAME = 'my-testing-bucket'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'doubtapi.storagebackends.MediaStorage'
